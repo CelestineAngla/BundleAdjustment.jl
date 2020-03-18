af4c8e48ce70dd60911cb972124f9fcda6af49b1
Célestine Angla <c-angla@orange.fr>
2020-03-18 16:32:18 +0100

    Fix bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Test
 include("../src/ReadFiles.jl")
-include("../src/Model.jl")
+include("../src/ModelJuMP.jl")
 
 @test Rodrigues_rotation([1.0, 1.0, 1.0], [2.5, -0.3, 1.0]) == [1.577353756980212, 2.1408840848258484, -0.5182378418060594]
 @test scaling_factor([1.0 1.0], 1.0, 1.0) == 7.0
