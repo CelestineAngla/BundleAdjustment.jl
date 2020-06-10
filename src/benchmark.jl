870c4b3a9a68c7c723ca07bc0eee125aeee31e46
Célestine Angla <c-angla@orange.fr>
2020-06-10 18:00:32 +0200

    Prepare benchmarks for ldl with normalization

--- src/benchmark.jl
+++ src/benchmark.jl
@@ -7,30 +7,10 @@ include("lm.jl")
 
 # Benchmarks for QR
 
-solvers = Dict(
-  :lmqrnone => model -> Levenberg_Marquardt(model, :QR, :AMD, :None),
-  :lmqrj => model -> Levenberg_Marquardt(model, :QR, :AMD, :J),
-  :lmqra => model -> Levenberg_Marquardt(model, :QR, :AMD, :A)
-)
-
-prob_names = ("LadyBug/problem-49-7776-pre.txt.bz2",
-              "LadyBug/problem-73-11032-pre.txt.bz2",
-              # "LadyBug/problem-138-19878-pre.txt.bz2",
-              "LadyBug/problem-318-41628-pre.txt.bz2",
-              "LadyBug/problem-460-56811-pre.txt.bz2",
-              # "LadyBug/problem-646-73584-pre.txt.bz2",
-              "LadyBug/problem-810-88814-pre.txt.bz2",
-              "LadyBug/problem-1031-110968-pre.txt.bz2",
-              # "LadyBug/problem-1235-129634-pre.txt.bz2",
-              "Dubrovnik/problem-202-132796-pre.txt.bz2"
-              )
-
-
-# Benchmarks or LDL
-
-# solvers = Dict(:lmldlnone=> model -> Levenberg_Marquardt(model,:LDL, :AMD, :None),
-#                :lmldlj => model -> Levenberg_Marquardt(model,:LDL, :AMD, :J),
-#                :lmldla => model -> Levenberg_Marquardt(model,:LDL, :AMD, :A)
+# solvers = Dict(
+#   :lmqrnone => model -> Levenberg_Marquardt(model, :QR, :AMD, :None),
+#   :lmqrj => model -> Levenberg_Marquardt(model, :QR, :AMD, :J),
+#   :lmqra => model -> Levenberg_Marquardt(model, :QR, :AMD, :A)
 # )
 #
 # prob_names = ("LadyBug/problem-49-7776-pre.txt.bz2",
@@ -42,25 +22,45 @@ prob_names = ("LadyBug/problem-49-7776-pre.txt.bz2",
 #               "LadyBug/problem-810-88814-pre.txt.bz2",
 #               "LadyBug/problem-1031-110968-pre.txt.bz2",
 #               # "LadyBug/problem-1235-129634-pre.txt.bz2",
-#               "Dubrovnik/problem-202-132796-pre.txt.bz2",
-#               "LadyBug/problem-1723-156502-pre.txt.bz2",
-#               "Dubrovnik/problem-273-176305-pre.txt.bz2",
-#               "Dubrovnik/problem-356-226730-pre.txt.bz2",
-#               "Venice/problem-427-310384-pre.txt.bz2",
-#               "Venice/problem-1350-894716-pre.txt.bz2"
+#               "Dubrovnik/problem-202-132796-pre.txt.bz2"
 #               )
+
+
+# Benchmarks or LDL
+
+solvers = Dict(:lmldlnone=> model -> Levenberg_Marquardt(model,:LDL, :AMD, :None),
+               :lmldlj => model -> Levenberg_Marquardt(model,:LDL, :AMD, :J),
+               :lmldla => model -> Levenberg_Marquardt(model,:LDL, :AMD, :A)
+)
+
+prob_names = ("LadyBug/problem-49-7776-pre.txt.bz2",
+              "LadyBug/problem-73-11032-pre.txt.bz2",
+              # "LadyBug/problem-138-19878-pre.txt.bz2",
+              "LadyBug/problem-318-41628-pre.txt.bz2",
+              "LadyBug/problem-460-56811-pre.txt.bz2",
+              # "LadyBug/problem-646-73584-pre.txt.bz2",
+              "LadyBug/problem-810-88814-pre.txt.bz2",
+              "LadyBug/problem-1031-110968-pre.txt.bz2",
+              # "LadyBug/problem-1235-129634-pre.txt.bz2",
+              "Dubrovnik/problem-202-132796-pre.txt.bz2",
+              # "LadyBug/problem-1723-156502-pre.txt.bz2",
+              "Dubrovnik/problem-273-176305-pre.txt.bz2",
+              "Dubrovnik/problem-356-226730-pre.txt.bz2",
+              "Venice/problem-427-310384-pre.txt.bz2",
+              "Venice/problem-1350-894716-pre.txt.bz2"
+              )
 
 problems = (FeasibilityResidual(BALNLPModel(name)) for name in prob_names)
 
 using Logging
-io = open("lm_norm_qr.log", "w")
+io = open("lm_norm_ldl.log", "w")
 stats = bmark_solvers(solvers, problems, solver_logger = Logging.ConsoleLogger(io))
 flush(io)
 close(io)
-save_stats(stats, "lm_stats_norm_qr.jld2")
+save_stats(stats, "lm_stats_norm_ldl.jld2")
 
 for solver in solvers
-  open(String(solver.first) * "_norm_qr.log","w") do io
+  open(String(solver.first) * "_norm_ldl.log","w") do io
     latex_table(io, stats[solver.first], cols=[:name, :nvar, :nequ, :objective, :elapsed_time, :iter,  :status, :dual_feas])
     markdown_table(io, stats[solver.first], cols=[:name, :nvar, :nequ, :objective, :elapsed_time, :iter,  :status, :dual_feas])
   end
@@ -80,4 +80,4 @@ costs = [stats -> .!solved(stats) .* Inf .+ stats.elapsed_time,
          stats -> .!solved(stats) .* Inf .+ stats.neval_jac_residual,
         ]
 profile_solvers(stats, costs, costnames)
-savefig("lm_profiles_norm_qr.pdf")
+savefig("lm_profiles_norm_ldl.pdf")
