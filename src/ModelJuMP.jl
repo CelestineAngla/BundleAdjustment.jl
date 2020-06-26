1e6aa2dfa6346f417e404f811bd4bbfe0f871d1c
Célestine Angla <c-angla@orange.fr>
2020-06-26 14:57:13 +0200

    Fix minor bug

--- src/ModelJuMP.jl
+++ src/ModelJuMP.jl
@@ -160,8 +160,8 @@ end
 
 
 # # Get cameras indices, point indices, points 2D, cameras and points 3D matrices for datasets
-cam_indices, pnt_indices, pt2d, cam_params, pt3d = readfile_JuMP("LadyBug/problem-49-7776-pre.txt.bz2")
+# cam_indices, pnt_indices, pt2d, cam_params, pt3d = readfile_JuMP("LadyBug/problem-49-7776-pre.txt.bz2")
 #
 # # Find optimal camera features and points
-cameras_opt, points_opt = direct_model(cam_indices, pnt_indices, pt2d, cam_params, pt3d)
+# cameras_opt, points_opt = direct_model(cam_indices, pnt_indices, pt2d, cam_params, pt3d)
 # cameras_opt, points_opt = residual_model(cam_indices, pnt_indices, pt2d, cam_params, pt3d)
