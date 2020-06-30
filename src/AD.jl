0096b4685d009a0e4ab186764613ebb8138e4d60
Célestine Angla <c-angla@orange.fr>
2020-06-30 09:49:42 +0200

    Add time comparison of AD

--- src/AD.jl
+++ src/AD.jl
@@ -3,18 +3,28 @@ include("BALNLPModels.jl")
 
 BA = BALNLPModel("LadyBug/problem-49-7776-pre.txt.bz2")
 model = FeasibilityResidual(BA)
+
 input = rand(model.meta.nvar)
 output = similar(input, model.nls_meta.nequ)
 r = (output, input) -> residual!(model, input, output)
+
 rows = Vector{Int}(undef, model.nls_meta.nnzj)
 cols = Vector{Int}(undef, model.nls_meta.nnzj)
 jac_structure_residual!(model, rows, cols)
 sparsity_pattern = Sparsity(model.nls_meta.nequ, model.meta.nvar, rows, cols)
 # sparsity_pattern = jacobian_sparsity(r, output, input)
 jac = Float64.(sparse(sparsity_pattern))  # replace Float64 with the precision being used
+
+# Automatic differentiation
+@time begin
 colors = matrix_colors(jac)
 forwarddiff_color_jacobian!(jac, r, model.meta.x0, colorvec = colors)
+end
 
+# Jacobian by hand
+@time begin
 vals = jac_coord_residual(model, model.meta.x0)
 j = sparse(rows, cols, vals)
+end
+
 print("\n", norm(j - jac))
